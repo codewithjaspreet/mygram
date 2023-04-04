13db959ef4a6acf102c9eb9151f020bfd905c8c5
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-04-04 13:32:35 +0530

    first commit

---
+++ src/Components/auth/Login.js
@@ -0,0 +1,57 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "./Login.css";
+import insta from '../../assets/insta.svg';
+import TextField from "@mui/material/TextField";
+import Alert from '@mui/material/Alert';
+
+export default function Login() {
+  return (
+    <div className="card__wrapper">
+      <div className="card__content">
+        <Card>
+          <div>
+            <img src={insta} alt="insta" className="logo" />
+          </div>
+          <CardContent>
+            <Typography variant="outlined" component="div">
+              Sign Up to see photos & videos from your friends.
+            </Typography>
+            {true && (
+              <Alert severity="error">
+                This is an error alert — check it out!
+              </Alert>
+            )}
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              fullWidth={true}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              fullWidth={true}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+              fullWidth={true}
+            />
+          </CardContent>
+
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </div>
+    </div>
+  );
+}
